87fdd155dfe0b5da8abce8c669b90d5bcffebad2
Jan Borowski <bor.jan.g.2018@gmail.com>
2025-12-11 17:57:52 +0100

    add test folder

---
+++ src/manga_processor/io/loaders/__init__.py
@@ -0,0 +1,5 @@
+from .mangaloader import MangaLoader, MangaNormalizer
+from .pageloader import PageLoader
+from .ocrresloader import OCRPageLoader, OCRResultLoader
+
+__all__ = ["MangaLoader", "MangaNormalizer", "PageLoader", "OCRPageLoader", "OCRResultLoader"]
---
+++ src/manga_processor/io/savers/__init__.py
@@ -0,0 +1,5 @@
+from .mangasaver import MangaSaver
+from .pagesaver import PageSaver
+from .ocrresaver import OCRPageSaver
+
+__all__ = ["MangaSaver", "PageSaver", "OCRPageSaver"]
